3c85a9ef32487fc2f815c93efb02b1e49f0a7a08
ColtonGarelli <coltongarelli@gmail.com>
2022-09-20 11:25:38 -0400

    Updates

--- src/components/Footer.js
+++
@@ -1,7 +0,0 @@
-import {MDBFooter} from "mdb-react-ui-kit";
-
-const Footer = () => {
-  console.log("foot");
-
-  return <MDBFooter />;
-};
--- src/store/constant.js
+++
@@ -1,4 +0,0 @@
-// theme constant
-export const gridSpacing = 5;
-export const drawerWidth = 350;
-export const appDrawerWidth = 320;
